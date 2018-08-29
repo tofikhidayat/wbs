5edbe91e72c364c87cca9f1a81b06d0abd4faf5d
tofik hidayat <topikhidayat711@gmail.com>
2018-08-29 19:20:51 +0700

    f

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,3 +157,34 @@ $(".next").click(function(){
     $(".carousel>.list:first-child").clone().appendTo(".carousel");
     $(".carousel>.list:first-child").remove();
 })
+
+
+
+$(document).ready(function(){
+
+  // Add smooth scrolling to all links
+  $("a.go").on('click', function(event) {
+
+    $(this).closest('ul').find('li').removeClass('active');
+    $(this).closest('li').addClass('active');
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
